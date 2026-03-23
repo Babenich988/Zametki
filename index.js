bd78009ed5c0b9c389a14aa6284208c843f93a3f
Babenich988 <denbab988@gmail.com>
2026-03-23 15:11:27 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let text=document.getElementById("text");
+let add=document.getElementById("add");
+let taskdel=document.getElementById("taskDelete");
+let del=document.getElementById("doneDelete");
+let tasksearch=document.getElementById("taskSearch");
+let search=document.getElementById("doneSearch");
