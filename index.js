975e161bf85b98bee664789d3d155383be7fc8ff
Babenich988 <denbab988@gmail.com>
2026-03-23 15:14:44 +0300

    feat: add search functionality for notes

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ let taskdel=document.getElementById("taskDelete");
 let del=document.getElementById("doneDelete");
 let tasksearch=document.getElementById("taskSearch");
 let search=document.getElementById("doneSearch");
+let tasks=[];
+let a=0;
+add.addEventListener("click", function () {
+    let text = document.getElementById("text").value;
+    addTask(text);
+});
+function addTask(text){
+    tasks.push({
+        id: a,
+        text: text,
+    });
+    a++;
+}
+///sdkjcbdskjbcdskbc
